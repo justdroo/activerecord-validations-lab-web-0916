5c33ccb05f04106625a2995a281fa11efe2ddce0
Flatiron School <flatironschool@Drew-2.local>
2016-10-25 10:41:45 -0400

    Completed and mostly Understood

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,13 +1,17 @@
 class Post < ActiveRecord::Base
-  validates :title, presence: true
+  # validates :title, presence: true
   validates :content, length: {minimum: 250}
   validates :summary, length: {maximum: 250}
   validates :category, inclusion: {in: %w(Fiction NonFiction)}
   validate :click_bait
 
   def click_bait
+    if self.title
       arr = self.title.split
       errors.add(:title, "not click-baity enough") unless arr.include?("Won't"||"Believe"||"Secret"||"Top"||"Guess")
+    else
+      errors.add(:title, "title does not exist")
+    end
   end
-  
+
 end
